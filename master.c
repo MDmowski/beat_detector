ce870d4f950bcd70e534aadc3f9374ebbd249317
Adam Szalowski <adamszalowski99@gmail.com>
2020-12-07 10:28:30 +0100

    Add comments to make it easier to understand

--- master.c
+++ master.c
@@ -27,7 +27,8 @@ static void read_msg(union sigval sv)
     printf("Received signal.\n");
     struct mq_attr attr;
     ssize_t bytes_received;
-    char *log_msg_buf;
+    char *buf;
+    struct log_msg *log_msg_buf;
     mqd_t mqd = *((mqd_t *) sv.sival_ptr);
 
     // Register for notify again
@@ -42,22 +43,28 @@ static void read_msg(union sigval sv)
 
     if (mq_getattr(mqd, &attr) == -1)
         handle_error("mq_getattr");
-    log_msg_buf = malloc(attr.mq_msgsize);
-    if (log_msg_buf == NULL)
+
+    buf = malloc(attr.mq_msgsize);
+    if (buf == NULL)
         handle_error("malloc");
 
+    // For notifes to work we have to empty the queue
     while(1)
     {
-        bytes_received = mq_receive(mqd, log_msg_buf, attr.mq_msgsize, NULL);
+        bytes_received = mq_receive(mqd, buf, attr.mq_msgsize, NULL);
         if (bytes_received != sizeof(struct log_msg))
         {
             if (errno != EAGAIN)
                 handle_error("mq_receive");
             else
-                break;
+                break; // Expected error, queue is empty
         }
-            
-        printf("Read %zd bytes from MQ: %u\n", bytes_received, ((struct log_msg *) log_msg_buf)->msg_id);
+
+        // To read contents of struct we have to cast it to appropiate type
+        log_msg_buf = (struct log_msg *) buf;
+
+        // Simple log, delete later
+        printf("Read %zd bytes from MQ: %u\n", bytes_received, log_msg_buf->msg_id);
     }
 
     free(log_msg_buf);
@@ -75,13 +82,11 @@ void mq_notify_wrapper(mqd_t *mqd_ptr)
         handle_error("mq_notify");
 }
 
-
-
 mqd_t mq_open_wrapper(const char *name)
 {
     mqd_t mqd = mq_open(name, O_CREAT | O_EXCL | O_RDONLY | O_NONBLOCK, S_IRUSR | S_IWUSR, NULL);
     if(mqd == (mqd) -1)
-        handle_error("mq_open(LOG_MSG_QUEUE_1)");
+        handle_error("mq_open");
 
     return mqd;
 }
@@ -93,15 +98,16 @@ int main()
     mqd_t mqd_log_1 = mq_open_wrapper("/LOG_MSG_QUEUE_1");
     mq_notify_wrapper(&mqd_log_1);
 
+    // Create first process
     pid_t pid = fork();
     if( pid == 0)
         execve("./p1.o", NULL, NULL);
 
 
+    // TODO: Add waiting for all children
     int status;
     wait(&status);
 
-    sleep(2);
     mq_close(mqd_log_1);
     mq_unlink("/LOG_MSG_QUEUE_1");
 
