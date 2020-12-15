ad9f9d6b702951524c37043dc1617be16ee8857b
Maciej Dmowski <maciej.dmowski.stud@pw.edu.pl>
2020-12-15 21:51:55 +0100

    Copying from queue to BTrack buffer

--- p2.c
+++ p2.c
@@ -33,19 +33,46 @@ int main()
     if (mq_getattr(mqd_1, &attr) == -1)
         handle_error("mq_getattr");
 
-    char *buf = malloc(attr.mq_msgsize);
+    char *buf = malloc(2048*sizeof(float));
     if (buf == NULL)
         handle_error("malloc");
-
+    double* frames = malloc(1024*sizeof(double));
+    if (frames == NULL)
+        handle_error("malloc");
+    uint frames_so_far = 0;
     while(1){
 
         ssize_t bytes_received = mq_receive(mqd_1, buf, attr.mq_msgsize, NULL);
+	uint frames_number = bytes_received/sizeof(float);
+	float* array = (float*)buf;
         printf("P2 bytes: %d\n", bytes_received);
-
+	uint free_space = 1024 - frames_so_far;
+	if(free_space >= frames_number){
+		for(int i=0; i < frames_number; i++)
+			frames[frames_so_far + i] = (double)array[i];
+		if(free_space == frames_number){
+			frames_so_far = 0;
+		//	BTrack
+		}
+		else
+			frames_so_far += frames_number;
+	}
+	else{
+		for(int i=0; i < free_space; i++)
+			frames[frames_so_far + i] = (double)array[i];
+		uint frames_left = frames_number - free_space;
+		//BTrack
+		for(int i=0; i < frames_left; i++)
+			frames[i] = (double)array[free_space + i];
+		frames_so_far = frames_left;
+	}
         printf("P2 got message\n");
-
+	//for(int i=0; i<1024; i++){
+	//	printf("c%d:%f ",i, frames[i]);
+	//}
+	//printf("\n\n");
         // Proccess data
-        
+	        
         // Send new data
         /* if(mq_send(mqd_2, (const char *)&new_msg, sizeof(struct p2_msg), 1) == -1) */
         /*     handle_error("mq_send"); */
